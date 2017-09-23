96301a69ea92765e8141fc96b2c7532cda53dd4f
CodeDraken <codedraken@gmail.com>
2017-09-22 20:28:25 -0500

    Start Google oauth

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send({ get: 'success' })
-})
+// auth with google
+passport.use(new GoogleStrategy())
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
